9654292f98a98d475676071c75cf7a3ae0e35879
dick braam <dickbraam@me.com>
2025-03-15 00:46:55 +0100

    Rename project van SUPERSET25 naar 5UPerS3t

---
+++ 5UPerS3tTests/5UPerS3tTests.swift
@@ -0,0 +1,17 @@
+//
+//  5UPerS3tTests.swift
+//  5UPerS3tTests
+//
+//  Created by dick braam on 01/03/2025.
+//
+
+import Testing
+@testable import 5UPerS3t
+
+struct 5UPerS3tTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ 5UPerS3tUITests/5UPerS3tUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  5UPerS3tUITestsLaunchTests.swift
+//  5UPerS3tUITests
+//
+//  Created by dick braam on 01/03/2025.
+//
+
+import XCTest
+
+final class 5UPerS3tUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

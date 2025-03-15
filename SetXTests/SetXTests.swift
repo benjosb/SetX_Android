c7a211dec8515e11d3c9edb4a93b3128c68ea6fc
dick braam <dickbraam@me.com>
2025-03-15 01:00:40 +0100

    Project hernoemd naar SetX

---
+++ SetXTests/SetXTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetXTests.swift
+//  SetXTests
+//
+//  Created by dick braam on 01/03/2025.
+//
+
+import Testing
+@testable import SetX
+
+struct SetXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
